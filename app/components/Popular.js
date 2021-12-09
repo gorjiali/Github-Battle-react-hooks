56c04f76bef39114a315a8f29677f970f3ae6da3
Ali Gorji <ag.aligorji@gmail.com>
2021-12-10 01:49:37 +0330

    all files

---
+++ app/components/Popular.js
@@ -0,0 +1,123 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from "react-icons/fa";
+import Card from "./Card";
+import Loading from "./Loading";
+import Tooltip from "./Tooltip";
+
+function LangaugesNav({ selected, onUpdateLanguage }) {
+  const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+  return (
+    <ul className="flex-center">
+      {languages.map((language) => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={language === selected ? { color: "rgb(187, 46, 31)" } : null}
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+LangaugesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired,
+};
+
+function ReposGrid({ repos }) {
+  return (
+    <ul className="grid space-around">
+      {repos.map((repo, index) => {
+        const { name, owner, html_url, stargazers_count, forks, open_issues } = repo;
+        const { login, avatar_url } = owner;
+
+        return (
+          <li key={html_url}>
+            <Card header={`#${index + 1}`} avatar={avatar_url} href={html_url} name={login}>
+              <ul className="card-list">
+                <li>
+                  <Tooltip text="Github username">
+                    <FaUser color="rgb(255, 191, 116)" size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
+                </li>
+                <li>
+                  <FaStar color="rgb(255, 215, 0)" size={22} />
+                  {stargazers_count.toLocaleString()} stars
+                </li>
+                <li>
+                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
+                  {forks.toLocaleString()} forks
+                </li>
+                <li>
+                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
+                  {open_issues.toLocaleString()} open
+                </li>
+              </ul>
+            </Card>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+ReposGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+};
+
+function fetchReducer(prevState, { type, value }) {
+  if (type === "success") {
+    return { ...prevState, [value.selectedLanguage]: value.data };
+  } else if (type === "error") {
+    return { ...prevState, error: value };
+  } else if (type === "start") {
+    return { ...prevState, error: null };
+  } else {
+    throw new Error("That type is not supported!");
+  }
+}
+
+export default function Popular() {
+  const [selectedLanguage, setSelectedLanguage] = React.useState("All");
+  const [fetchState, dispatch] = React.useReducer(fetchReducer, { error: null });
+
+  React.useEffect(() => {
+    updateLanguage(selectedLanguage);
+  }, [selectedLanguage]);
+
+  const updateLanguage = (selectedLanguage) => {
+    setSelectedLanguage(selectedLanguage);
+    dispatch({ type: "start" });
+
+    if (!fetchState[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then((data) => {
+          dispatch({ type: "success", value: { selectedLanguage, data } });
+        })
+        .catch(() => {
+          dispatch({ type: "error", value: "There was an error fetching the repositories." });
+        });
+    }
+  };
+  const isLoading = () => !fetchState[selectedLanguage] && fetchState.error === null;
+
+  return (
+    <React.Fragment>
+      <LangaugesNav selected={selectedLanguage} onUpdateLanguage={updateLanguage} />
+
+      {isLoading() && <Loading text="Fetching Repos" />}
+
+      {fetchState.error && <p className="center-text error">{fetchState.error}</p>}
+
+      {fetchState[selectedLanguage] && <ReposGrid repos={fetchState[selectedLanguage]} />}
+    </React.Fragment>
+  );
+}
